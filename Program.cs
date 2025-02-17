8abff822f01d7ecffd32b3ed28fafb2a42dd18c4
EdwardNkosi <sithembison@lightstone.co.za>
2025-02-18 00:58:38 +0200

    Initial commit - Day One Challenge

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {userName}!");
+    }
+}
